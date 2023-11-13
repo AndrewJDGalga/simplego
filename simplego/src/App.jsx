e7445c076aa6baf7193a87b2f4e0dd59021f9f4f
Andrew DGalga <ex.spake@gmail.com>
2023-11-12 22:12:42 -0600

    do I really need a separate file for visual grid when it won't be doing anything save look pretty? Putting in app file for now, might move later.

--- simplego/src/App.jsx
+++ simplego/src/App.jsx
@@ -1,9 +1,23 @@
 import Cell from './Cell';
 import './css_prod/style.css';
 
+function GridBox() {
+  return (
+    <div className='gridbox'></div>
+  )
+}
+
+function VisualBoxGrid() {
+
+  return (
+    <div></div>
+  )
+}
+
 export default function App() {
   return (
     <main className='gospace'>
+      <GridBox />
       <Cell />
     </main>
   )
