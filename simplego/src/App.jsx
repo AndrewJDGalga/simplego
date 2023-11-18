1996f2e6c8d932940373cf9ab40fd36c90ad69b2
Andrew DGalga <ex.spake@gmail.com>
2023-11-17 20:23:32 -0600

    current player tracker.

--- simplego/src/App.jsx
+++ simplego/src/App.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import GameBoard from './components/GameBoard';
 import './css_prod/style.css';
 
 
 export default function App() {
+  const [curPlayer, setCurPlayer] = useState(1);
+
+  function switchPlayer() {
+    setCurPlayer(prev=> (prev === 1) ? 2 : 1);
+  }
+
   return (
     <main className='gospace'>
+      <p>Current Player {curPlayer}</p>
       <GameBoard />
+      <button id='end-turn' onClick={switchPlayer}>End Turn</button>
     </main>
   )
 }
