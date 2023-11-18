46a0c9286b320aff35013001eaf03c1ecb3498cc
Andrew DGalga <ex.spake@gmail.com>
2023-11-17 19:46:00 -0600

    moved board into its own component.

---
+++ simplego/src/components/GameBoard.jsx
@@ -0,0 +1,49 @@
+import Cell from "./Cell";
+
+function GridBox() {
+    return (
+      <div className='gridbox'></div>
+    )
+  }
+  
+  /*generate a grid using a set size and React component*/
+  function genGrid(size, ReactObj) {
+    const tempArr = [];
+    for(let i = 0; i < size; i++) {
+      tempArr.push(<ReactObj key={i} />);
+    }
+    return tempArr;
+  }
+  
+  function VisualBoxGrid({boardSize}) {
+    //conversion for easier values: 9x9 board actualy 8x8 squares, 13x13 = 12, etc
+    const converted = boardSize - 1;
+    //set square grid to match size
+    document.documentElement.style.setProperty('--board-size-sqrs', `${converted}`);
+    const visualArray = genGrid(converted * converted, GridBox);
+    return (
+      <div className='gridbox-container'>
+        {visualArray}
+      </div>
+    )
+  }
+  
+  function CellGrid({boardSize}) {
+    const cellArray = genGrid(boardSize * boardSize, Cell);
+    //set dot grid to match size
+    document.documentElement.style.setProperty('--board-size-dots', `${boardSize}`);
+    return (
+      <div className='cellgrid'>
+        {(cellArray.length > 0 && cellArray)}
+      </div>
+    )
+  }
+
+export default function GameBoard() {
+    return (
+        <div className='gospace-boardhost'>
+            <CellGrid boardSize={9} />
+            <VisualBoxGrid boardSize={9} />
+      </div>
+    )
+}
