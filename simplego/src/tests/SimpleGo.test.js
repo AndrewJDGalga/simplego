0a0e000f3d5680dce2f8b7cddf596bffac800354
Andrew DGalga <ex.spake@gmail.com>
2023-12-13 16:22:09 -0600

    alot of this may not actually need to be tested, but testing anyway for experience and because I enjoy it.

--- simplego/src/tests/SimpleGo.test.js
+++ simplego/src/tests/SimpleGo.test.js
@@ -8,15 +8,20 @@ describe("generate stone button", ()=>{
     let currentPlayer = 0;
     const stone = document.createElement("button");
     stone.id = "stone";
-
     stone.addEventListener('click', ()=>{
         if(stone.classList.length === 0) stone.className = currentPlayer === 0 ? "black" : "white";
     })
     document.body.innerHTML = stone;
 
-    test("button press to set type", ()=>{
+    test("button press to set type black", ()=>{
         stone.click();
         expect(stone.className).toBe("black");
+    })
+    test("button press to set type white", ()=>{
+        stone.className = "";
+        currentPlayer++;
+        stone.click();
+        expect(stone.className).toBe("white");
     })
     test("button press fail once type set", ()=>{
         stone.click();
@@ -25,17 +30,30 @@ describe("generate stone button", ()=>{
 })
 
 describe("generate 9x9 board", ()=>{
-    test.todo("generated board");
-    test.todo("generate board with 9x9 squares");
+    let currentPlayer = 0;
+    const arr = [];
+
+    for(let y = 0; y < 9; y++){
+        arr.push([]);
+        for(let x = 0; x < 9; x++) {
+            const stone = document.createElement("button");
+            arr[y].push(stone);
+            stone.id = `stone${x.toString() + y.toString()}`
+            stone.addEventListener('click', ()=>{
+                if(stone.classList.length === 0) stone.className = currentPlayer === 0 ? "black" : "white";
+            });
+            document.body.innerHTML = stone;
+        }
+    }
+
+    test("generated 9x9 array", ()=>{
+        let count = 0;
+        for(let y = 0; y < arr.length; y++) {
+            for(let x = 0; x < arr[y].length; x++){
+                count++;
+            }
+        }
+        expect(count).toBe(81);
+    });
 });
 
-/*
-describe("capturing", ()=>{
-    let type = 'toad';
-    let testObj = {
-        type:'toad',
-    }
-    test('object matches provided type', ()=>{
-        expect(isType(testObj, type)).toBe(true);
-    })
-});*/
